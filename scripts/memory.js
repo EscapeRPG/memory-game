488588b63f4fef01aef8ef3a2bb5da2cbef0f439
EscapeRPG <syrphin@syrphin.com>
2025-05-05 16:10:23 +0200

    implement main script

---
+++ scripts/memory.js
@@ -0,0 +1,8 @@
+import { currentPage } from "./nav.js";
+
+// import { display } from "./displayer";
+// import { buildDeck } from "./deckBuilder";
+
+const board = document.getElementById("board");
+
+window.addEventListener("load", currentPage);
